086fc6bce8b54a84cf0559fb6742f8a5fb6fe3e2
DevPieter <51260315+DevPieter@users.noreply.github.com>
2022-12-11 15:16:27 +0100

    Initial

---
+++ Color o Magic v2/InteractionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Color_o_Magic_v2;
+
+public class InteractionHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly InteractionService _interactionService;
+    private readonly IServiceProvider _serviceProvider;
+
+    public InteractionHandler(DiscordSocketClient client, InteractionService interactionService, IServiceProvider serviceProvider)
+    {
+        _client = client;
+        _interactionService = interactionService;
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+        _client.InteractionCreated += OnInteractionCreated;
+    }
+
+    private async Task OnInteractionCreated(SocketInteraction interaction)
+    {
+        try
+        {
+            var context = new SocketInteractionContext(_client, interaction);
+            await _interactionService.ExecuteCommandAsync(context, _serviceProvider);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ Color o Magic v2/Program.cs
@@ -0,0 +1,79 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Color_o_Magic_v2;
+
+public class Program
+{
+    public static Task Main() => new Program().MainAsync();
+
+    private async Task MainAsync()
+    {
+        // Load the environment variables from the .env file
+        DotNetEnv.Env.Load();
+
+        // Setup dependency injection
+        using var host = Host
+            .CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services
+                    .AddSingleton(x => new DiscordSocketClient())
+                    .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                    .AddSingleton<InteractionHandler>();
+            }).Build();
+
+        // Run the client asynchronously
+        await RunClientAsync(host);
+    }
+
+    private async Task RunClientAsync(IHost host)
+    {
+        using var serviceScope = host.Services.CreateScope();
+        var provider = serviceScope.ServiceProvider;
+
+        var client = provider.GetRequiredService<DiscordSocketClient>();
+        var config = provider.GetRequiredService<IConfiguration>();
+
+        // Initialize the interaction handler
+        var interactions = provider.GetRequiredService<InteractionService>();
+        await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+
+        // Log messages to the console
+        client.Log += LogAsync;
+        interactions.Log += LogAsync;
+
+        client.Ready += async () =>
+        {
+            Console.WriteLine("Client is ready!");
+            // Register the application interactions
+            await interactions.RegisterCommandsToGuildAsync(UInt64.Parse(config["TEST_SERVER"] ?? ""));
+            //await interactions.RegisterCommandsGloballyAsync();
+        };
+
+        // Login and connect to Discord
+        await client.LoginAsync(TokenType.Bot, config["TOKEN"]);
+        await client.StartAsync();
+
+        // Set the activity of the bot
+        await client.SetActivityAsync(ParseGame(config["ACTIVITY"], config["ACTIVITY_TYPE"]));
+
+        await Task.Delay(-1);
+    }
+
+    private Task LogAsync(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private Game ParseGame(string? activity, string? type)
+    {
+        return new Game(activity ?? "", Enum.TryParse(type, true, out ActivityType activityType) ? activityType : ActivityType.Playing);
+    }
+}
