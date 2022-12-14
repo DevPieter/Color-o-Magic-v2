21e725ec7186f00b0783591e9c2013e1f1334249
DevPieter <51260315+DevPieter@users.noreply.github.com>
2022-12-14 13:44:47 +0100

    Added GetColorInfoEmbed() to EmbedTemplates.cs

--- Color o Magic v2/Templates/EmbedTemplates.cs
+++ Color o Magic v2/Templates/EmbedTemplates.cs
@@ -1,6 +1,17 @@
+using System.Drawing;
+using Color_o_Magic_v2.Utils;
+
 namespace Color_o_Magic_v2.Templates;
 
 public static class EmbedTemplates
 {
-    
+    public static Discord.EmbedBuilder GetColorInfoEmbed(Color color)
+    {
+        var embed = EmbedHelper.GetColoredEmbed(color);
+        embed.AddField("Hex", $"`{color.ToHexString()}`", true);
+        embed.AddField("Rgb", $"`{color.ToRgbString()}`", true);
+        embed.AddField("Argb", $"`{color.ToArgbString()}`", true);
+
+        return embed;
+    }
 }
